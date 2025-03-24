c14ce850c73c85537e15fa33402d0ba72e380b6d
Dhananjay Navlani <dhananjay.navlani@novumlogic.com>
2025-03-24 12:37:11 +0530

    Refactor dependencies and update STUN server configuration: remove unused dependencies from build.gradle.kts and libs.versions.toml, and add additional STUN servers in MyPeerConnectionFactory.kt

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -126,20 +126,6 @@ android {
     }
 }
 
-dependencies {
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(compose.uiTooling)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-}
 
 compose.desktop {
     application {
